68acc4ef02d9782eb6ceae22ec0399b4722a4a79
nunupy345345 <airinota@outlook.jp>
2024-03-10 15:21:37 +0900

    ページ遷移、pnf作成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route,Rocation} from "react-router-dom";
+
+import { Pnf } from "./pages/pnf";
+import { Start } from "./pages/start";
+import { Play } from "./pages/play";
+import { Result } from "./pages/result";
+import {Explain} from "./pages/explain";
+import {Company} from "./pages/company";
+import {Coupon} from "./pages/coupon";
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+    <BrowserRouter>
+      <ul>
+      </ul>
+      <Routes>
+        <Route activeClassName="active" path="*"element={<Pnf />}/>
+        <Route activeClassName="active" path="/"element={<Start />}/>
+        <Route activeClassName="active" path="/start"element={<Start />}/>
+        <Route activeClassName="active" path="/play"element={<Play />}/>
+        <Route activeClassName="active" path="/result"element={<Result />}/>
+        <Route activeClassName="active" path="/explain"element={<Explain/>}/>
+        <Route activeClassName="active" path="/company"element={<Company/>}/>
+        <Route activeClassName="active" path="/explain"element={<Explain/>}/>
+        <Route activeClassName="active" path="/coupon"element={<Coupon/>}/>
+        <Route path='/play' exact component={Play} />
+        <Route path='/result' component={Result}/>
+        
+      </Routes>
+    </BrowserRouter>
+  </div>
   );
 }
 
---
+++ src/pages/pnf.js
@@ -0,0 +1,11 @@
+import {NavLink} from "react-router-dom";
+export const Pnf = () => {
+  return (
+    <div className="pnf">
+       
+      <h1>ページが見つかりません</h1>
+      <NavLink to="/">ホームに戻る</NavLink>
+
+    </div>
+  );
+};
